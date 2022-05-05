b97b0823be180a437303e02b40723ebaed5df35c
F88\sangnt3 <sangnt3@f88.vn>
2022-05-05 19:01:54 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,187 @@
+let slide_index = 0;
+let slide_play = true;
+let slides = document.querySelectorAll(".slide");
+
+hideAllSlide = () => {
+  slides.forEach((e) => {
+    e.classList.remove("active");
+  });
+};
+
+showSlide = () => {
+  hideAllSlide();
+  slides[slide_index].classList.add("active");
+};
+
+nextSlide = () =>
+  (slide_index = slide_index + 1 === slides.length ? 0 : slide_index + 1);
+
+prevSlide = () =>
+  (slide_index = slide_index - 1 < 0 ? slides.length - 1 : slide_index - 1);
+
+// pause slide when hover slider
+document
+  .querySelector(".slider")
+  .addEventListener("mouseover", () => (slide_play = false));
+
+// enable slide when mouse leave out slider
+document
+  .querySelector(".slider")
+  .addEventListener("mouseleave", () => (slide_play = true));
+
+// slider controll
+
+document.querySelector(".slide-next").addEventListener("click", () => {
+  nextSlide();
+  showSlide();
+});
+
+document.querySelector(".slide-prev").addEventListener("click", () => {
+  prevSlide();
+  showSlide();
+});
+
+showSlide();
+
+setInterval(() => {
+  if (!slide_play) return;
+  nextSlide();
+  showSlide();
+}, 3000);
+
+// render products
+
+let products = [
+  {
+    name: "JBL E55BT KEY BLACK",
+    image1: "./images/x1.jpg",
+    image2: "./images/JBL_LIVE650BTNC_Product Image_Folded_Black.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "JBL JR 310BT",
+    image1: "./images/x2.jpg",
+    image2: "./images/JBL_JR 310BT_Product Image_Detail_Skyblue.png",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "JBL TUNE 750BTNC",
+    image1: "./images/x1.jpg",
+    image2: "./images/JBL_E55BT_KEY_RED_6063_FS_x1-1605x1605px.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "JBL Horizon",
+    image1: "./images/x3.jpg",
+    image2: "./images/JBLHorizon_BLK_002_dvHAMaster.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "JBL Tune 220TWS",
+    image1: "./images/x4.jpg",
+    image2: "./images/JBL_TUNE220TWS_ProductImage_Pink_Back.png",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "UA Project Rock",
+    image1: "./images/x5.jpg",
+    image2:
+      "./images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_BACK.png",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "JBL Endurance SPRINT",
+    image1: "./images/x5.jpg",
+    image2: "./images/JBL-Endurance-Sprint_Alt_Red-1605x1605px.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+];
+
+let product_list = document.querySelector("#latest-products");
+let best_product_list = document.querySelector("#best-products");
+
+products.forEach((e) => {
+  let prod = `
+        <div class="col-3 col-md-6 col-sm-12">
+            <div class="product-card">
+                <div class="product-card-img">
+                    <img src="${e.image1}" alt="">
+                </div>
+                <div class="product-card-info">
+                    <div class="product-btn">
+                        <button class="btn-flat btn-hover btn-shop-now">Mua ngay</button>
+                        <button class="btn-flat btn-hover btn-cart-add">
+                            <i class='bx bxs-cart-add'></i>
+                        </button>
+                        <button class="btn-flat btn-hover btn-cart-add">
+                            <i class='bx bxs-heart'></i>
+                        </button>
+                    </div>
+                    <div class="product-card-name">
+                        ${e.name}
+                    </div>
+                    <div class="product-card-price">
+                        <span><del>${e.old_price}</del></span>
+                        <span class="curr-price">${e.curr_price}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+  product_list.insertAdjacentHTML("beforeend", prod);
+  best_product_list.insertAdjacentHTML("afterbegin", prod);
+});
+
+$(".slick-test").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+$(".product-card").click(function () {
+  window.location.href = window.location.origin + "/product-detail.html";
+});
+
+// function ipLookUp() {
+//   $.ajax("http://ip-api.com/json").then(
+//     function success(response) {
+//       console.log("User's Location Data is ", response);
+//       console.log("User's Country", response.country);
+//     },
+
+//     function fail(data, status) {
+//       console.log("Request failed.  Returned status of", status);
+//     }
+//   );
+// }
+// ipLookUp();
